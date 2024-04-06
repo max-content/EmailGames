0af64882c88e60f46fd66594b6b7e61458befc25
Max Salzman <mnsalz91@gmail.com>
2024-04-06 13:37:09 -0400

    trying again

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/EmailGames/client/dist",
   plugins: [react()],
-  // base: "EmailGames/client/dist",
 })
 
